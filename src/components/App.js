283488a1dc8a6f9f79a792e3b379ea8f557f8e45
jfkbeatkid <hernadezrigoberto@gmail.com>
2018-12-06 10:25:55 -0800

    refactored logic out of APP component

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import PlantList from './PlantList';
-import API from '../api';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      plants: []
-    }
-  }
-  async componentDidMount()  {
-    const res = await API.get();
-    this.setState({ plants: res.data })
-  }
-  render() {
-    return (
-      <div className="ui container" style={{marginTop: "20px"}}>
-
-        <PlantList plants={this.state.plants} />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="ui container" style={{marginTop: "20px"}}>
+      <PlantList />
+    </div>
+  )
 }
 
 export default App;
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import PlantCard from './PlantCard';
+import API from '../api';
 
 
-const PlantList = ({plants}) => {
-  const getPlants = () => {
-    return plants.map(plant => <PlantCard plant={plant} key={plant._id} />);
+class PlantList extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      plants: []
+    }
   }
-  return (
-    <div className="ui list">
-      {getPlants()}
-    </div>
-  )
+  async componentDidMount() {
+    const res = await API.get();
+    this.setState({ plants: res.data })
+  }
+  getPlants() {
+    return this.state.plants.map(plant => <PlantCard plant={plant} key={plant._id} />);
+  }
+  render() {
+    return (
+      <div className="ui list">
+        {this.getPlants()}
+      </div>
+    )
+  }
 }
 
 export default PlantList;
